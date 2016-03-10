507a53c811c3e3df4d8d93df208ad85e88a6dd87
sarinomu <xxkotetsuxx@gmail.com>
2016-03-10 09:40:11 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ var cookies = 0;
 var clickers = 0;
 var clickPower = 0;
 var clickerPower = 0;
+var base = 1;
+var test = 0;
 
-function cookieClick(number)
+if(clickPower < 1)
 {
-	if(clickPower < 1)
+	document.getElementById("click").onclick = function() {cookieClick(base, clickPower)};
+}
+else if(clickPower >=1)
+{
+	//test = (base * clickPower);
+	document.getElementById("click").onclick = function() {cookieClick(base, clickPower)};
+}
+
+function cookieClick(number, powerUp)
+{
+	document.getElementById("checking2").innerHTML = powerUp;
+	if(powerUp < 1)
 	{
-		cookies += number;
+		test = number;
 	}
-	else if(clickPower)
+	else if(powerUp > 1)
 	{
-		cookies += (number * clickPower);
+		test = (number * powerUp);
 	}
+	
+	document.getElementById("check2").innerHTML = number;
+	cookies += test;
 	document.getElementById("cookies").innerHTML = cookies;
 }
 
@@ -45,9 +61,12 @@ function sellClicker()
 
 function incPower()
 {
-	if(cookies >= 0)
+	if(cookies >= 10)
 	{
+		cookies -= 10;
+		document.getElementById("cookies").innerHTML = cookies;
 		clickPower += 2;
+		document.getElementById("checking").innerHTML = clickPower;
 	}
 }
 
@@ -59,11 +78,18 @@ function incClickerPower()
 	}
 }
 
-if(clickerPower <= 0)
-{
-	window.setInterval(function(){cookieClick((clickers));},1000)	
-}
-else
-{
-	window.setInterval(function(){cookieClick((clickers * clickerPower));},1000)
-}
+//if(clickers > 0)
+//{
+	if(clickerPower < 1)
+	{
+		window.setInterval(function(){
+			cookieClick(clickers, 0);
+		},1000);
+	}
+	else if(clickerPower > 1)
+	{
+		window.setInterval(function(){
+			cookieClick(clickers, clickerPower);
+		},1000);	
+	}	
+//}
